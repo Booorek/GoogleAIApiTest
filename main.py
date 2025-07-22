199767601c82b99c6f474f0e50b2843971d42fbf
Booorek <bartosz.sedzikowski31@gmail.com>
2025-07-23 01:41:23 +0200

    Messages history

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,10 +12,14 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
-    print(
-        f"Prompt tokens: {res.usage_metadata.prompt_token_count}\nResponse tokens: {res.usage_metadata.candidates_token_count}")
-    print(res.text)
+    messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])])]
+
+    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {sys.argv[1]}\nPrompt tokens: {res.usage_metadata.prompt_token_count}\nResponse tokens: {res.usage_metadata.candidates_token_count}")
+    else:
+        print(res.text)
+
 
 if __name__ == "__main__":
     main()
