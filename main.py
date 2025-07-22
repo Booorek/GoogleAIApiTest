7027e220d942d4433b783261e705525b1d9dadd6
Booorek <bartosz.sedzikowski31@gmail.com>
2025-07-23 01:23:37 +0200

    Response from google api

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+    print(
+        f"Prompt tokens: {res.usage_metadata.prompt_token_count}\nResponse tokens: {res.usage_metadata.candidates_token_count}")
+    print(res.text)
+
+if __name__ == "__main__":
+    main()
